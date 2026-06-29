067bf6774731acb1b20fa8c8b42937372497b135
drewk312 <drewk312@users.noreply.github.com>
2026-06-28 21:02:58 -0600

    chore: add project scaffold, gradle build files, and .gitignore

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+plugins {
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+rootProject.name = "AudiophilePlayer"
+if (System.getenv("DESKTOP_ONLY") == "true") {
+    include(":desktopApp")
+}
+include(":app")
+include(":shared")
+
